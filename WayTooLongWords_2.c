5bdb8da3ba99c1e9fe2abe0f9abec847b6b9a433
asifiwe-oss <mucyoasifiwe80@gmail.com>
2026-01-15 21:37:36 +0200

    Update WayTooLongWords_2.c

--- WayTooLongWords_2.c
+++ WayTooLongWords_2.c
@@ -1,13 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
 int main(){
-    char n[100];
-    int i;
-    scanf("%s",&n);
-     for(i = 0; i <= 99; i++){
-        int j = n[i];
-     if(i5 >= 9){
-        printf("%c %d %c\n",n[0],j,n[i]);
-     }
-    }
-    return 0;
+   char word[101];
+   int len;
+   int n;
+   scanf("%d",&n);
+   for(int i = 0; i < n; i++){
+   scanf("%s",word);
+   len = strlen(word);
+   if(len > 10){
+    printf("%c%d%c\n",word[0],len-2,word[len-1]);
+   }
+   else{
+      printf("%s\n",word);
+   }
+  }
+   return 0;
 }
