6fed66ddd8ddedb5502ce8145e8ecd7a90c9d5ed
asifiwe-oss <mucyoasifiwe80@gmail.com>
2026-01-15 20:01:42 +0200

    Add files via upload

---
+++ WayTooLongWords_2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    char n[100];
+    int i;
+    scanf("%s",&n);
+     for(i = 0; i <= 99; i++){
+        int j = n[i];
+     if(i5 >= 9){
+        printf("%c %d %c\n",n[0],j,n[i]);
+     }
+    }
+    return 0;
+}
